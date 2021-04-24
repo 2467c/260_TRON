fede7b660ce9d3be4c206e1098a481ed4a128b4e
Ankit <ankit2467c@gmail.com>
2021-04-24 15:21:14 +0200

    Engine for the game

---
+++ src/main/java/pv260/general/Engine.java
@@ -0,0 +1,13 @@
+package pv260.general;
+
+import java.awt.*;
+
+public interface Engine {
+
+    void game_start();
+    void game_stop();
+    void game_update(long Passed_time);
+    void draw(Graphics2D g);
+    
+
+}
